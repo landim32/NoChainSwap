919019844ef6dff597b4e1bf6eec48b10602a26b
rodrigo.carneiro <rodrigo.carneiro@benner.com.br>
2024-10-31 10:18:19 -0300

    clean frontend

--- install_docker.bat
+++ install_docker.bat
@@ -4,4 +4,6 @@ docker network create docker-network
 docker network connect docker-network postgres1
 docker build -t nochainswap-api -f NoChainSwap.API\Dockerfile .
 docker run --name nochainswap-api1 -p 8080:8080 --network docker-network nochainswap-api &
-docker run --name nochainswap-api1 -p 8080:80 --network docker-network nochainswap-api
+
+docker build -t nochainswap-app .
+docker run --name nochainswap-app1 -p 80:3001 nochainswap-app
