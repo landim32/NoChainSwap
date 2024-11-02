49a1a5d5274da1ba63d872a527c7f86fce2f92a1
rodrigo.carneiro <rodrigo.carneiro@benner.com.br>
2024-11-02 16:32:41 -0300

    deploy front

--- install_docker.bat
+++ install_docker.bat
@@ -3,7 +3,7 @@ docker network inspect docker-network
 docker network create docker-network
 docker network connect docker-network postgres1
 docker build -t nochainswap-api -f NoChainSwap.API\Dockerfile .
-docker run --name nochainswap-api1 -p 8080:8080 --network docker-network nochainswap-api &
+docker run --name nochainswap-api1 -p 8080:8080 -e ASPNETCORE_URLS="https://+" -e ASPNETCORE_HTTPS_PORTS=8080 --network docker-network nochainswap-api &
 
 docker build -t nochainswap-app .
 docker run --name nochainswap-app1 -p 80:3001 nochainswap-app
