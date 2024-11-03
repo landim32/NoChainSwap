7320879fc0f17a21f1f67ff75d1043e0d345ac50
rodrigo.carneiro <rodrigo.carneiro@benner.com.br>
2024-11-02 23:42:04 -0300

    deploy front

--- Backend/NoChainSwap/DB.Infra/Context/NoChainSwapContext.cs
+++ Backend/NoChainSwap/DB.Infra/Context/NoChainSwapContext.cs
@@ -22,8 +22,7 @@ public partial class NoChainSwapContext : DbContext
     public virtual DbSet<User> Users { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseNpgsql("Host=167.172.240.71;Port=5432;Database=crosschainswap;Username=postgres;Password=eaa69cpxy2");
+        => optionsBuilder.UseNpgsql("Host=172.18.0.2;Port=5432;Database=crosschainswap;Username=postgres;Password=eaa69cpxy2");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
